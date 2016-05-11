2d6e65041a4f3e48cd510fe99dff1f8c9d953bba
Robert Stovall <rochovian@gmail.com>
2016-05-11 01:33:44 -0500

    0.20

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    [System.Serializable]
+    public class EnemyStats
+    {
+        private int _currentHealth;        
+
+        public int maxHealth = 100;
+        public int currentHealth
+        {
+            get { return _currentHealth; }
+            set
+            {
+                _currentHealth = Mathf.Clamp(value, 0, maxHealth);
+            }
+        }
+
+        public int damage = 40;
+
+        public void Init()
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public EnemyStats stats = new EnemyStats();
+    public Transform deathParticles;
+
+    public float shakeAmount = 0.1f;
+    public float shakeLength = 0.1f;
+
+    [Header("Optional: ")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    void Start()
+    {
+        stats.Init();
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
+        if (deathParticles == null)
+        {
+            Debug.LogError("No Dp referenced on enemy");
+        }
+    }
+
+    public void DamageEnemy(int damage)
+    {
+        stats.currentHealth -= damage;
+        if (stats.currentHealth <= 0)
+        {
+            GameMaster.KillEnemy(this);
+        }
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D colliderInfo)
+    {
+        Player player = colliderInfo.collider.GetComponent<Player>();
+        if (player != null)
+        {
+            player.DamagePlayer(stats.damage);
+            DamageEnemy(99999);
+        }
+
+    }
+}
---
+++ Assets/Scripts/StatusIndicator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour {
+    
+    [SerializeField]
+    private RectTransform healthBarRect;
+    [SerializeField]
+    private Text healthText;
+
+    void Start()
+    {
+        if (healthBarRect == null)
+        {
+            Debug.LogError("STATUS INDICATOR: No health bar reference");
+        }
+        if (healthText == null)
+        {
+            Debug.LogError("STATUS INDICATOR: No health bar reference");
+        }
+    }
+
+    public void SetHealth(int currentHealth, int maxHealth)
+    {
+        float value = (float)currentHealth / maxHealth;
+        //should change health bar color on damage in the future using a Gradient 
+
+        healthBarRect.localScale = new Vector3(value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+        healthText.text = currentHealth + "/" + maxHealth + " HP";
+    }
+}
