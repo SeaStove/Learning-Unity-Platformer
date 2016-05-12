89749335a5ad5df84d527b21354759f1e2974252
Robert Stovall <rochovian@gmail.com>
2016-05-12 03:00:09 -0500

    0.25

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public Scene sceneToLoad;
+
+    public void Quit()
+    {
+        Debug.Log("APPLICATION QUIT");
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public enum SpawnState
+    {
+        SPAWNING, WAITING, COUNTING
+    };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+    public int NextWave
+    {
+        get { return nextWave + 1; }
+    }
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5f;
+    private float waveCountdown = 0f;
+    public float WaveCountdown
+    {
+        get { return waveCountdown; }
+    }
+
+    SpawnState state = SpawnState.COUNTING;
+
+    private float searchCountdown = 1f;
+
+    public SpawnState State
+    {
+        get { return state; }
+    }
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawnpoint");
+        }
+    }
+
+    void Update()
+    {
+
+        if (state == SpawnState.WAITING)
+        {
+            //check if enemies are still alive
+            if (!EnemyIsAlive())
+            {
+                //begin a new round
+                WaveCompleted();
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                //start spawning wave
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave wave)
+    {
+        //Debug.Log("Spawning wave: " + wave.name);
+        state = SpawnState.SPAWNING;
+
+        //spawn
+        for (int i = 0; i < wave.count; i++)
+        {
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave completed");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            //add difficulty later
+            nextWave = 0;
+            Debug.Log("All waves complete! Looping");
+        }
+        else
+        {
+            nextWave++;
+        }
+
+    }
+
+    void SpawnEnemy(Transform enemy)
+    {
+        Transform sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(enemy, sp.position, sp.rotation);
+        //Debug.Log("Spawning enemy" + enemy.name);
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
